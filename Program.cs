50d03c17addc4c130a061e447637f56038ff09b5
Kenzodiac <kennethfujimura@gmail.com>
2022-10-20 16:03:54 -0700

    Final push

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 //Last accessed: 10-19-2022
 //GitHub Challenge # 7 - Reverse It
 //This program takes in a number input from the user and then reverses it and reports that back to the user.
-//Peer reviewed by:
+//Peer reviewed by: Peer Reviewed By: Reed Goodwin- THis is a well written and well thought out. You can se the effort put into it if you look into the code. The UI is well desgned and makes the experience interesting. I liked it
 
 Console.Clear();
 
